7dc608a63666d6fd334169e20374b96c2333571f
Kartheeshwaran .G <122508393+Kartheeshmca@users.noreply.github.com>
2025-10-04 13:16:39 +0530

    Update sendemail.js

--- Utils/sendemail.js
+++ Utils/sendemail.js
@@ -10,11 +10,16 @@ export const sendEmail = async (to, subject, text, html = null) => {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+       tls: { rejectUnauthorized: false }, // Important for Render
+        connectionTimeout: 10000, // 10 seconds timeout
+
     });
 
     // Verify connection (optional for debugging)
     // await transporter.verify();
     await transporter.verify();
+        console.log("✅ SMTP connection verified");
+
     await transporter.sendMail({
       from: `"Boutique App" <${process.env.SMTP_USER}>`,
       to,
@@ -22,7 +27,7 @@ export const sendEmail = async (to, subject, text, html = null) => {
       text,
       html: html || undefined, // if html is provided, it will be used
     });
-
+    console.log("Email sent successfully to", to);
   } catch (error) {
     console.error("Error sending email:", error.message);
     throw new Error("Email could not be sent");
